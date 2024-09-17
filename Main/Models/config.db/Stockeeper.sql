982718d5e00c0514a240bd559fb1a424479c2156
Jarom Kepri de Sousa Marques <khepry.coder@gmail.com>
2024-09-16 23:20:50 -0300

    up

--- Main/Models/config.db/Stockeeper.sql
+++ Main/Models/config.db/Stockeeper.sql
@@ -3,7 +3,7 @@ CREATE TABLE "produto" (
   "nome" TEXT NOT NULL, -- No postgress é usado Varchar no lugar do text --
   "preco" REAL NOT NULL, -- No postgress é usado Decimal no lugar do real --
   "quantidade" INTEGER NOT NULL,
-  "categoria_id" INTEGER,
+  "categoria_id" INTEGER NULL,
   FOREIGN KEY ("categoria_id") REFERENCES "categoria" ("id")
 );
 
@@ -11,9 +11,9 @@ CREATE TABLE "compra" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
   "nome" TEXT NOT NULL,
   "total" REAL NOT NULL,
-  "fornecedor_id" INTEGER,
-  "responsavel_id" INTEGER,
-  "produto_id" INTEGER,
+  "fornecedor_id" INTEGER NULL,
+  "responsavel_id" INTEGER NULL,
+  "produto_id" INTEGER NULL,
   data_compra DATE NOT NULL, -- No postgress é usado DATE no lugar do text --
   FOREIGN KEY ("fornecedor_id") REFERENCES "fornecedor" ("id"),
   FOREIGN KEY ("responsavel_id") REFERENCES "funcionario" ("id"),
@@ -34,7 +34,7 @@ CREATE TABLE "funcionario" (
 CREATE TABLE "fornecedor" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
   "nome" TEXT NOT NULL,
-  "categoria_produtos_id" INTEGER,
+  "categoria_produtos_id" INTEGER NULL,
   FOREIGN KEY ("categoria_produtos_id") REFERENCES "categoria" ("id")
 );
 
@@ -43,8 +43,8 @@ CREATE TABLE "venda" (
   "quantidade" INTEGER NOT NULL,
   "total" REAL NOT NULL,
   "data_venda" TEXT NOT NULL,
-  "responsavel_id" INTEGER,
-  "produto_id" INTEGER,
+  "responsavel_id" INTEGER NULL, 
+  "produto_id" INTEGER NULL,
   FOREIGN KEY ("responsavel_id") REFERENCES "funcionario" ("id"),
   FOREIGN KEY ("produto_id") REFERENCES "produto" ("id")
 );
