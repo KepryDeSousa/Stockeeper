bb6a5c82e7f8590a930b1ef349665907248344a9
Jarom Kepri de Sousa Marques <khepry.coder@gmail.com>
2024-09-18 03:25:34 -0300

    up

--- xmls/xmls.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-Banco_load = pd.read_excel('xmls/Efas_para_fazer.xlsx', index_col=0)
-Banco_load2 = pd.read_excel('xmls/Efas_para_fazer.xlsx', index_col=0, sheet_name='Planilha2')
-print(Banco_load.head())
-print(Banco_load2.head())
-
-dt = pd.merge(Banco_load, Banco_load2, on='Loja', how='inner')
-print(dt.head())
